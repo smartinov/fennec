00ebf4cb4d541904aeb135cfe77d7868e7216a60
smartinov <stefan.martinov@danulabs.com>
2014-05-25 21:12:12 +0200

    removed unused CSS removed useless lines of code

--- fennec/apps/web/views.py
+++ fennec/apps/web/views.py
@@ -3,7 +3,4 @@ from django.contrib.auth.decorators import login_required
 
 @login_required
 def dashboard(request):
-    if request.user.is_authenticated():
-        return render_to_response('web/dashboard.html')
-    else:
-        return redirect('/auth/sign-in?continueTo=%s' % request.path)
+    return render_to_response('web/dashboard.html')
