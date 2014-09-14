fc25e513391f92bd10ab72e8153a471294d4f83a
smartinov <stefan.martinov@danulabs.com>
2014-09-14 21:28:39 +0200

    - changed to rest easy for easier access - moved javascript around - removed unused imports

--- fennec/apps/web/static/web/js/dashboard/controllers.js
+++ fennec/apps/web/static/web/js/dashboard/controllers.js
@@ -1,38 +1,42 @@
-var projectsRoot = '/api/projects/'
-app = angular.module('fennec.dashboard', ['mgcrea.ngStrap','ngAnimate'])
+var projectsRoot = '/api/projects/';
+var app = angular.module('fennec.dashboard', ['mgcrea.ngStrap', 'ngAnimate', 'ngResource']);
 
-app.config(function($alertProvider) {
-      angular.extend($alertProvider.defaults, {
+app.factory('Projects', function ($resource) {
+    return $resource('/api/projects/:id'); // Note the full endpoint address
+});
+app.config(['$httpProvider', function ($httpProvider) {
+    $httpProvider.defaults.headers.common['X-CSRFToken'] = '{{ csrf_token|escapejs }}';
+}]);
+app.config(function ($alertProvider) {
+    angular.extend($alertProvider.defaults, {
         animation: 'am-fade-and-slide-top',
         placement: 'top-right',
         duration: 3,
         container: '#alert-container'
-      });
-    }).controller("ProjectsController", ['$scope','$http','$alert',
-    function ($scope, $http, $alert) {
+    });
+});
+app.controller("ProjectsController",
+    function ($scope, $http, $alert, Projects) {
         $scope.projects = [];
         $scope.reload = function () {
-            $http.get(projectsRoot).success(function (data, status) {
-                $scope.projects = data;
-            });
+            $scope.projects = Projects.query();
         }
 
-        $scope.create = function(name) {
-           project = {
-              "name":name,
-              "description":"No description provided",
-              "created_by":"http://127.0.0.1:8000/api/users/1/"
-           }
+        $scope.create = function (name) {
+            project = {
+                "name": name,
+                "description": "No description provided",
+                "created_by": "http://127.0.0.1:8000/api/users/1/"
+            }
 
-           data = angular.toJson(project);
-           $http.post(projectsRoot,data)
-           .success(function (data,status) {
-                $scope.reload();
-                $scope.project_name = '';
-                var myAlert = $alert({title: 'Success!', content: 'Project creation successful!   ', type: 'info', show: true});
-           }).error(function (data,status) {
+            data = angular.toJson(project);
+            $http.post(projectsRoot, data)
+                .success(function (data, status) {
+                    $scope.reload();
+                    $scope.project_name = '';
+                    var myAlert = $alert({title: 'Success!', content: 'Project creation successful!   ', type: 'info', show: true});
+                }).error(function (data, status) {
 
-           });
+                });
         }
-
-    }]);
+    });
--- fennec/apps/web/urls.py
+++ fennec/apps/web/urls.py
@@ -1,5 +1,3 @@
-from django.conf.urls import patterns, include, url
-
 from django.conf.urls import patterns, url, include
 
 urlpatterns = patterns(
