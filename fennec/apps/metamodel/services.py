0b8846aeec7970efa774d40dcdb9aa3440cfe9e2
smartinov <stefan.martinov@danulabs.com>
2015-03-08 20:56:55 +0100

    refactored method, added comment for python auto-completion

--- fennec/apps/metamodel/services.py
+++ fennec/apps/metamodel/services.py
@@ -1,10 +1,26 @@
 from StringIO import StringIO
+
 from rest_framework.parsers import JSONParser
+
 from fennec.apps.metamodel.serializers import SchemaSerializer, NamespaceSerializer, TableSerializer, ColumnSerializer, \
     IndexSerializer, ForeignKeySerializer, DiagramSerializer, LayerSerializer, TableElementSerializer, \
     RelationshipElementSerializer
 
+
 __author__ = 'stefan.martinov@gmail.com'
+
+serializer_mappings = {
+    'Schema': SchemaSerializer,
+    'Namespace': NamespaceSerializer,
+    'Table': TableSerializer,
+    'Column': ColumnSerializer,
+    'Index': IndexSerializer,
+    'ForeignKey': ForeignKeySerializer,
+    'Diagram': DiagramSerializer,
+    'Layer': LayerSerializer,
+    'TableElement': TableElementSerializer,
+    'RelationshipElement': RelationshipElementSerializer
+}
 
 
 def convert_change_to_object(change):
@@ -22,23 +38,9 @@ def convert_change_to_object(change):
     except Exception as e:
         pass
 
-    serializer = switch_type(change.object_type)(data=data)
+    serializer_type = serializer_mappings[change.object_type]
+    serializer = serializer_type(data=data)
     if not serializer.is_valid():
         print serializer.errors
 
     return serializer.object
-
-
-def switch_type(object_type):
-    return {
-        'Schema': SchemaSerializer,
-        'Namespace': NamespaceSerializer,
-        'Table': TableSerializer,
-        'Column': ColumnSerializer,
-        'Index': IndexSerializer,
-        'ForeignKey': ForeignKeySerializer,
-        'Diagram': DiagramSerializer,
-        'Layer': LayerSerializer,
-        'TableElement': TableElementSerializer,
-        'RelationshipElement': RelationshipElementSerializer
-    }.get(object_type, None)
