a515a22de12aa211938091e3e5a365e39d31d7b8
sklebecko <klebecko@gmail.com>
2015-06-02 23:23:44 +0200

    - added diagrams (tabs, only visually )

--- fennec/apps/diagram/static/js/diagram/controllers/fennecCtrl.js
+++ fennec/apps/diagram/static/js/diagram/controllers/fennecCtrl.js
@@ -7,6 +7,31 @@
     var module = angular.module('myApp.controllers')
         .controller('DiagramController',  function($scope, $filter, $http, diagramService){
 
+            $scope.tabs = [{
+			id:1,
+			content:'This is a default tab on load'
+		}]
+
+		$scope.counter = 1;
+		/** Function to add a new tab **/
+		$scope.addTab = function(){
+			$scope.counter++;
+			$scope.tabs.push({id:$scope.counter,content:'Any Content'});
+			$scope.selectedTab = $scope.tabs.length - 1; //set the newly added tab active.
+		}
+
+		/** Function to delete a tab **/
+		$scope.deleteTab = function(index){
+			$scope.tabs.splice(index,1); //remove the object from the array based on index
+		}
+
+		$scope.selectedTab = 0; //set selected tab to the 1st by default.
+
+		/** Function to set selectedTab **/
+		$scope.selectTab = function(index){
+			$scope.selectedTab = index;
+		}
+
             // TODO: get from url branchRevisionId
             // TODO: 1. load project_state(diagram,schemas) this is data
             //       2. Load the selected diagram elements(symbols)
