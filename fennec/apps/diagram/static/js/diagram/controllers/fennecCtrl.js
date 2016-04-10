17287031188054dd31d943517587ac0489a0fa69
klebecko@gmail.com <CXm3ZhgfRfMB>
2016-04-10 23:31:37 +0200

    minor refactoring

--- fennec/apps/diagram/static/js/diagram/controllers/fennecCtrl.js
+++ fennec/apps/diagram/static/js/diagram/controllers/fennecCtrl.js
@@ -5,8 +5,26 @@
         .controller('DiagramController', function ($scope, $filter ,$location,$log,
                                                    diagramService, spinnerService,Notification) {
 
+
             init();
             function init() {
+                $scope.panels = [
+  {
+    "title": "Collapsible Group Item #1",
+    "body": "Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch."
+  },
+  {
+    "title": "Collapsible Group Item #2",
+    "body": "Food truck fixie locavore, accusamus mcsweeney's marfa nulla single-origin coffee squid. Exercitation +1 labore velit, blog sartorial PBR leggings next level wes anderson artisan four loko farm-to-table craft beer twee."
+  },
+  {
+    "title": "Collapsible Group Item #3",
+    "body": "Etsy mixtape wayfarers, ethical wes anderson tofu before they sold out mcsweeney's organic lomo retro fanny pack lo-fi farm-to-table readymade."
+  }
+];
+$scope.panels.activePanel = -1;
+
+                $scope.isCollapsed   =true;
                 $scope.projectInfo = {};
                 $scope.schemas = [];
                 $scope.activeSchema = {};
